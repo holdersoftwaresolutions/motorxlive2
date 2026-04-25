210631b83437ad200b586d3b752f6307c5506599
Caleb Holder <caleb@holdersoftwaresolutions.com>
2026-04-25 18:49:06 -0500

    Fix YouTube discovery import

--- api/src/youtube-discovery/youtube-discovery.module.ts
+++ api/src/youtube-discovery/youtube-discovery.module.ts
@@ -2,8 +2,11 @@ import { Module } from "@nestjs/common";
 import { YouTubeDiscoveryController } from "./youtube-discovery.controller";
 import { YouTubeDiscoveryService } from "./youtube-discovery.service";
 import { YouTubeClient } from "./youtube-client";
+import { PrismaModule } from "../prisma/prisma.module";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [YouTubeDiscoveryController],
   providers: [YouTubeDiscoveryService, YouTubeClient],
   exports: [YouTubeDiscoveryService],
